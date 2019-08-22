9fa2f46dad8953ed648a2ad4ebe87de0c398b58b
sinkline <popovartem505@gmail.com>
2019-08-22 11:47:25 +0300

    lesson 18 dz step 1

--- ruby artem/projects/Lesson18/file.rb
+++ ruby artem/projects/Lesson18/file.rb
@@ -1,56 +1,30 @@
 require 'net/http'
 require 'uri'
 
-def wrong_pass password
-	uri = URI.parse 'https://accounts.google.com/signin/v2/sl/pwd?hl=ru&passive=true&continue=https%3A%2F%2Fwww.google.com%2F%3Fhl%3Dru&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
-	response = Net::HTTP.post_form(uri, :username => "popovartem505@gmail.com", :password => password).body
-	response.include? "denied"
+def wrong_pass? password
+	uri = URI.parse 'http://localhost:4567/login'
+	response = Net::HTTP.post_form(uri, :username => "admin", :password => password).body
+	response.include? "Wrong username or password, please try again"
 end
 
 
-puts wrong_pass ",pl.[by[jkmw2003"
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
-input = File.open "Password.txt", "r"  
-
-	while line = input.gets
+input = File.open "Password.txt", "r" 
+	
+while (line = input.gets)
 		line.strip!
 		print "password: ...#{line}..."
 		if wrong_pass? line
 			puts "Error"
-			sleep 0.40
+			
 		else
 			puts "It's OK"
-			sleep 0.40
 			
+			input.close
 			exit
 		end
-	end
-input.close
-=end
+		
+end
+

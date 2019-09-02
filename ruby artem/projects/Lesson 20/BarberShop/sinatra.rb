09cb2106d8edd2717f0b85eb0e518130ab8ab4a4
sinkline <popovartem505@gmail.com>
2019-09-02 17:55:01 +0300

    create user.txt

--- ruby artem/projects/Lesson 20/BarberShop/sinatra.rb
+++ ruby artem/projects/Lesson 20/BarberShop/sinatra.rb
@@ -10,7 +10,11 @@ post '/' do
 	@datetime = params[:datetime]
 
 	@title = "Thank you!"
-	@message = "Hello, '#{@username}' your application has been sent to '#{@datetime}'."
+	@message = "Hello, #{@username} your application has been sent to '#{@datetime}'."
+
+	f = File.open 'user.txt', 'a'
+	f.write "User: #{@username}, Phone: #{@phone}, Date and time: #{@datetime}.\n"
+	f.close
 
 	erb :message
 end
